b1e08835d947aba1a5ee9f512dd84f494d6b354a
章刚 <370637594@qq.com>
2020-06-28 14:07:22 +0800

    1

--- ch2/src/main/java/com/kangfoo/nettystudy/ch2/_2_1_BIO/TimeServerHandler.java
+++ ch2/src/main/java/com/kangfoo/nettystudy/ch2/_2_1_BIO/TimeServerHandler.java
@@ -39,7 +39,7 @@ public class TimeServerHandler implements Runnable {
                 }
 
                 System.out.println("The time server receive order : " + body);
-                currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new Date(System.currentTimeMillis()).toString() : "BAD ORDER";
+                currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new Date(System.currentTimeMillis()).toString() +" hello server" : "BAD ORDER";
                 out.println(currentTime);
             }
 
